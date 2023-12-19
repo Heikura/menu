c199effbb4d658d6c0fd92d5edd7dc59944e351a
Arttu Heikura <arttu.heikura@dainstudios.com>
2023-12-19 21:55:12 +0200

    fix: typo fixed.

--- menu/src/components/Extras.js
+++ menu/src/components/Extras.js
@@ -4,7 +4,7 @@ export default function Extras({ type, items }) {
   return (
     <section className="extras">
       <h2 className="extras-heading">{type}</h2>
-      <div className="extras-desc"> Menuumme on saatavilla juomapaketti, joka sisältää alkumaljan, alkoholittoman ipan alkuruoalle ja pääruoalle. Jälkiruoan kruunaa tarjoiltu espresso. Lisäksi juomapakettiin sisältyy rajaton määrä hanavettä.</div>
+      <div className="extras-desc"> Menuumme on saatavilla juomapaketti, joka sisältää alkumaljan, sekä alkoholittoman ipan alkuruoalle ja pääruoalle. Jälkiruoan kruunaa tarjoiltu espresso. Lisäksi juomapakettiin sisältyy rajaton määrä hanavettä.</div>
     </section>
   );
 }
